54cdab917a5bf48513e909c7de6c068b76fd8ee3
abhi-sriram <65218890+abhi-sriram@users.noreply.github.com>
2020-08-11 12:32:45 +0530

    Add files via upload
    Solving sudoku using python recursive.

---
+++ sudoku.py
@@ -0,0 +1,61 @@
+
+
+grid = [
+        [0,0,0,0,9,1,4,0,0],
+        [0,0,0,7,0,0,0,0,0],
+        [5,0,3,0,0,4,9,0,0],
+        [7,0,4,0,0,0,0,5,0],
+        [9,0,0,0,2,0,0,0,1],
+        [0,2,0,0,0,0,6,0,4],
+        [0,0,6,3,0,0,0,0,7],
+        [0,0,0,0,7,6,0,0,0],
+        [0,0,7,8,1,0,0,0,0]
+        ]
+
+def print_grid():
+  global grid
+  for i in range(9):
+    for j in range(9):
+      print(grid[i][j],end=' ')
+    print()
+
+
+def check_row_col_mat(x,y,n):
+  global grid
+
+  # traversing through row and checking for number existence
+  for i in range(9):
+    if(grid[x][i]==n):
+      return False
+
+  # traversing through column and checking for number existence
+  for j in range(9):
+    if(grid[j][y]==n):
+      return False
+
+  # checking 3x3 matrix 
+  x0 = (x//3)*3
+  y0 = (y//3)*3
+  for i in range(3):
+    for j in range(3):
+      if(grid[x0+i][y0+j]==n):
+        return False
+
+  return True
+
+def solve():
+  global grid
+  for r in range(9):
+    for c in range(9):
+      if(grid[r][c]==0):
+        for n in range(1,10):
+          if(check_row_col_mat(r,c,n)):
+            grid[r][c] = n
+            solve()
+            grid[r][c] = 0
+        return
+  print_grid()
+  input('more?')
+
+solve()
+
